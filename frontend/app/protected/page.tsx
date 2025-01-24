48b55efff328a616fdbaf1efc4dba6ef1c78bf08
HZauto <hzahin14@gmail.com>
2025-01-25 01:22:34 +0600

    user-namage

--- frontend/app/protected/page.tsx
+++ frontend/app/protected/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import Link from "next/link"
 
 export default function Dashboard() {
     return (
@@ -16,15 +17,17 @@ export default function Dashboard() {
             {/* Dashboard Sections */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
                 {/* User Management Section */}
-                <Card className="w-full">
-                    <CardHeader>
-                        <CardTitle>User Management</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="text-sm text-gray-500">Manage users, view profiles, and handle user access.</p>
-                    </CardContent>
-                </Card>
-
+                <Link
+                    href="user-manage">
+                    <Card className="w-full">
+                        <CardHeader>
+                            <CardTitle>User Management</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <p className="text-sm text-gray-500">Manage users, view profiles, and handle user access.</p>
+                        </CardContent>
+                    </Card>
+                </Link>
                 {/* Station Management Section */}
                 <Card className="w-full">
                     <CardHeader>
